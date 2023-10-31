69106952c22f2864cfafb5bc173c70e3efcc46d3
Anton Pakhomov <apahomov@kolos.studio>
2023-10-31 16:59:07 +0300

    add new files

---
+++ Sources/SPMBlago/Happy.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nebo on 31.10.2023.
+//
+
+import Foundation
+
+public class Happy {
+    
+    public static func log(string: String) {
+        print("Blago----\(string)")
+    }
+    
+}
--- Sources/SPMBlago/SPMBlago.swift
+++ Sources/SPMBlago/SPMBlago.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+public class Blago {
+    
+    public static func log(string: String) {
+        print("Blago----\(string)")
+    }
+    
+    
+}
